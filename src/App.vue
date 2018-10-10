6d6c8730eb694606feb15248bc9eed9a491e519f
Sebastien Crapoulet <sebastien.crapoulet@capita.co.uk>
2018-10-10 09:42:27 +0100

    (initial commit) Added template

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about" class="text-red">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="postcss">
+  @import 'assets/styles/tailwind.postcss';
+</style>
